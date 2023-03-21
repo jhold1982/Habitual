b31ec530272230904cbdf29f735f6370c9c4fd61
Justin Hold <36420615+jhold1982@users.noreply.github.com>
2023-03-21 15:35:26 -0500

    Initial Commit

---
+++ Habitual/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Habitual
+//
+//  Created by Justin Hold on 3/21/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Habitual/HabitualApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitualApp.swift
+//  Habitual
+//
+//  Created by Justin Hold on 3/21/23.
+//
+
+import SwiftUI
+
+@main
+struct HabitualApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
